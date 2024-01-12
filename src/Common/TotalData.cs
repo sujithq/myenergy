21cb5604654c85d4b3c1f3a207c69f0c33a60dee
Sujith Quintelier <sujith.quintelier@gmail.com>
2024-01-12 15:56:33 +0100

    Show 2 decimal digits in totals
    [no ci]

--- src/Common/TotalData.cs
+++ src/Common/TotalData.cs
@@ -1,4 +1,4 @@
 ﻿namespace myenergy.Common
 {
-    public record TotalData(int TotalProduction, int TotalNetInjection, int TotalNetUsage, int TotalUsage);
+    public record TotalData(double TotalProduction, double TotalNetInjection, double TotalNetUsage, double TotalUsage);
 }
