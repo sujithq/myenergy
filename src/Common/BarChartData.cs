48a08dbfa1baa290511bc26f36a4bb37c13d3289
Sujith Quintelier <sujith.quintelier@gmail.com>
2024-01-17 16:39:23 +0100

    Show Anomaly Data

--- src/Common/BarChartData.cs
+++ src/Common/BarChartData.cs
@@ -3,4 +3,6 @@
     public record BarChartData(int D, double P, double U, double I, bool J, bool S, MeteoStatData MS, bool M, AnomalyData AS);
 
     public record AnomalyData(double P, double U, double I, bool A);
+
+    public record AnomalyModalData(int Y, int D, AnomalyData A);
 }
